a92343f7e8fd4a15f26be32688777fb4e74dbec2
rwd-github <rwd-github>
2019-03-12 12:23:35 +0100

    -

--- buildNrun.sh
+++ buildNrun.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
+set -o errexit -o pipefail -o nounset
 
 mypath=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-docker build -t ubuntu-firefox ${mypath}
-#xhost +local:
-docker run -ti --rm -e DISPLAY=$DISPLAY \
--v /tmp/.X11-unix:/tmp/.X11-unix \
--v /home/user1/AlienFirefoxProfile/profile:/home/user/.mozilla \
---name ubuntu-firefox \
---hostname ubuntu-firefox \
-ubuntu-firefox
+imagetag=ubuntu-firefox
+stdparams=""
+additionalparams=""
+
+function build {
+	docker build ${stdparams} -t ${imagetag} ${mypath}
+}
+
+function run {
+	docker run -it --rm ${stdparams} \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /home/user1/AlienFirefoxProfile/profile:/home/user/.mozilla \
+	--name ${imagetag} --hostname ${imagetag} ${additionalparams} ${imagetag} 
+}
+
+
+case $1 in
+	b)
+	echo "build"
+	build
+	;;
+	r)
+	echo "run"
+	run
+	;;
+	br)
+	build && run
+	;;
+	bash)
+	additionalparams="--entrypoint /bin/bash"
+	run
+	;;
+	*)
+	echo "unbekanntes Kommando: $1"
+	;;
+esac
+
 
 
